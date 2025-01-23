917a38a5907960e1214bced4e684439a45027e72
nolann levanen <levanen.n@stjolorient.fr>
2025-01-23 10:37:20 +0100

    modif client relais

--- UDP/client_udp_relais.c
+++ UDP/client_udp_relais.c
@@ -8,12 +8,15 @@
 #include <string.h>
 #include <netinet/in.h>
 
+int traiter_commande_wrapper(int argc, char** argv) {
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+	
+}
+
 int main(int argc, char** argv) {
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
 	
-	/*A COMPLETER*/
-	
-	exit(0);
+	traiter_commande_wrapper(argc, argv);
+
 }
