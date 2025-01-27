1b0693efd9e717091e4715a7ba7f34260df9b770
User1 <nalbo.j@stjolorient.fr>
2025-01-27 11:24:59 +0100

    Modification erreur udp.c et code udp.h completer

--- UDP/udp.c
+++ UDP/udp.c
@@ -11,12 +11,15 @@
 void creer_socket(char* adresseIP, int port, SOCK* sock) {
     // Créer une socket UDP
     sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    traiter_erreur(__FUNCTION__);
+    if (sock->sockfd < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
     
     // Initialiser la structure d'adresse
     memset(&sock->addr, 0, sizeof(sock->addr));
     sock->addr.sin_family = AF_INET;
     sock->addr.sin_port = htons(port);
+    sock->addrlen = sizeof(struct sockaddr_in);
     
     // Si adresseIP est NULL, on utilise INADDR_ANY
     if (adresseIP == NULL) {
@@ -28,30 +31,30 @@ void creer_socket(char* adresseIP, int port, SOCK* sock) {
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
-    int result = bind(sock->sockfd, (struct sockaddr*)&sock->addr, sizeof(sock->addr));
-    traiter_erreur(__FUNCTION__);
+    int result = bind(sock->sockfd, (struct sockaddr*)&sock->addr, sock->addrlen);
+    if (result < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
 }
 
-/*Initialiser la structure adresse client */
+/* Initialiser la structure adresse client */
 void init_addr(SOCK* sock) {
     memset(&sock->addr, 0, sizeof(sock->addr));
     sock->addr.sin_family = AF_INET;
-    sock->addr_len = sizeof(sock->addr);
+    sock->addrlen = sizeof(struct sockaddr_in);
 }
 
 /* Dimensionner la file d'attente d'une socket */
 void dimensionner_file_attente_socket(int taille, SOCK* sock) {
     // Cette fonction n'est pas nécessaire pour UDP car il n'y a pas de file d'attente
-    // UDP est sans connexion
     (void)taille; // Pour éviter l'avertissement de variable non utilisée
     (void)sock;   // Pour éviter l'avertissement de variable non utilisée
-    traiter_erreur(__FUNCTION__);
 }
 
 /* Recevoir un message */
 void recevoir_message(SOCK* dst, char* buffer) {
-    ssize_t nbytes = recvfrom(dst->sockfd, buffer, TAILLE_BUFFER, 0,
-                             (struct sockaddr*)&dst->addr, &dst->addr_len);
+    ssize_t nbytes = recvfrom(dst->sockfd, buffer, TAILLE_BUFFER - 1, 0,
+                             (struct sockaddr*)&dst->addr, &dst->addrlen);
     if (nbytes < 0) {
         traiter_erreur(__FUNCTION__);
     }
@@ -61,7 +64,7 @@ void recevoir_message(SOCK* dst, char* buffer) {
 /* Émettre un message */
 void envoyer_message(SOCK* dst, char* message) {
     ssize_t nbytes = sendto(dst->sockfd, message, strlen(message), 0,
-                           (struct sockaddr*)&dst->addr, sizeof(dst->addr));
+                           (struct sockaddr*)&dst->addr, dst->addrlen);
     if (nbytes < 0) {
         traiter_erreur(__FUNCTION__);
     }
